613c15f1b585149b28187d3b6784b93f80534c1f
Sam Frank <sfrank2147@gmail.com>
2013-12-27 09:04:51 -0600

    matches multi-line titles

--- part1/script1.py
+++ part1/script1.py
@@ -17,8 +17,7 @@ documents = [DIV_COMM, MAG_CART]
 # the metadata headers at the top of Project gutenberg docs, and we want to account 
 # for possibility of "title: Some Title", "Title: Some Title", and "TITLE: Some Title").
 #title_search = re.compile(r'(title:\s*)(?P<title>.*)', re.IGNORECASE)
-title_search = re.compile(r'(title:\s*)(?P<title>.*(\n.*\w.*)*)', re.IGNORECASE 
-                                                                + re.MULTILINE)
+title_search = re.compile(r'(title:\s*)(?P<title>.*(\n.*\w.*)*)', re.IGNORECASE)
 author_search = re.compile(r'(author:)(?P<author>.*)', re.IGNORECASE)
 translator_search = re.compile(r'(translator:)(?P<translator>.*)', re.IGNORECASE)
 illustrator_search = re.compile(r'(illustrator:)(?P<illustrator>.*)', re.IGNORECASE)
